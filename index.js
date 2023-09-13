2d5dfbee1ca388d15d42ccdfa35bd399a81914e4
Ben Johnson <monk.sound.design@gmail.com>
2023-09-13 19:41:33 -0400

    server wired

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // start your server here
+const server = require("./api/server");
+
+const PORT = process.env.PORT || 9000;
+
+server.use("*", (req, res) => {
+  res.json("Hello world");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
