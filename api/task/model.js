50ce03a4bfb730f2bfbdedc62b7d21679f21553f
Ben Johnson <monk.sound.design@gmail.com>
2023-09-14 01:02:59 -0400

    fixed task GET query and POST return as boolean

--- api/task/model.js
+++ api/task/model.js
@@ -5,17 +5,43 @@ const getById = (id) => {
   return db("tasks").where("task_id", id);
 };
 
+// SELECT
+// p.project_id,
+// project_name,
+// project_description,
+// project_completed,
+// task_id,
+// task_description,
+// task_notes,
+// task_completed
+//     FROM projects as p
+//     JOIN tasks as t
+//     ON p.project_id = t.project_id;
+
 const getAll = async () => {
-  const tasks = await db("tasks");
-  tasks.forEach((task) => {
+  const rows = await db("projects as p")
+    .join("tasks as t", "p.project_id", "t.project_id")
+    .select(
+      "p.project_id",
+      "project_name",
+      "project_description",
+      "task_id",
+      "task_description",
+      "task_notes",
+      "task_completed"
+    );
+  rows.forEach((task) => {
     task.task_completed = task.task_completed === 0 ? false : true;
   });
-  return tasks;
+  return rows;
 };
 
 const insert = async (task) => {
   const [id] = await db("tasks").insert(task);
-  return getById(id).first();
+  const newTask = await getById(id).first();
+  newTask.task_completed = newTask.task_completed =
+    newTask.task_completed === 0 ? false : true;
+  return newTask;
 };
 
 module.exports = {
