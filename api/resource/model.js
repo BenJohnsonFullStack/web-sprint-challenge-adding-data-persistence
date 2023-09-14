1e55fbfb01fee6fd4bfd58f4f067297fb424dc39
Ben Johnson <monk.sound.design@gmail.com>
2023-09-13 23:15:22 -0400

    insert model function

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,15 @@
 // build your `Resource` model here
+const db = require("../../data/dbConfig");
+
+const getById = (id) => {
+  return db("resources").where("resource_id", id);
+};
+
+const insert = async (resource) => {
+  const [id] = await db("resources").insert(resource);
+  return getById(id).first();
+};
+
+module.exports = {
+  insert,
+};
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,10 +1,39 @@
 // build your `/api/resources` router here
 const express = require("express");
+const Resource = require("./model");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.json("Hello from resources");
+});
+
+router.post("/", async (req, res, next) => {
+  const { resource_id } = req.params;
+  const { resource_name, resource_description } = req.body;
+  try {
+    const result = await Resource.insert({
+      resource_id,
+      resource_name,
+      resource_description,
+    });
+    res.status(201).json(result);
+  } catch (err) {
+    next(err);
+  }
+  const resource = await resource.insert({
+    resource_id,
+    resource_name,
+    resource_description,
+  });
+});
+
+// eslint-disable-next-line no-unused-vars
+router.use((err, req, res, next) => {
+  res.json({
+    status: err.status || 500,
+    message: err.message,
+  });
 });
 
 module.exports = router;
