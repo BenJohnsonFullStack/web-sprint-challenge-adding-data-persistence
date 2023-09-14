1de862a7c86cc0bfec3d314208ddfcecb16bb3b8
Ben Johnson <monk.sound.design@gmail.com>
2023-09-13 20:02:00 -0400

    knexfile, migration and seed init

---
+++ data/seeds/01-cleaner.js
@@ -0,0 +1,12 @@
+const cleaner = require("knex-cleaner");
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  await cleaner.clean(knex, {
+    mode: "truncate", // resets ids
+    ignoreTables: ["knex_migrations", "knex_migrations_lock"], // don't empty migration tables
+  });
+};
