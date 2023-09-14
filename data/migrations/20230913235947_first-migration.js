ff8df9e0d627de05a9a70b0b665e8123283c8cc2
Ben Johnson <monk.sound.design@gmail.com>
2023-09-13 22:38:17 -0400

    created tables

--- data/migrations/20230913235947_first-migration.js
+++ data/migrations/20230913235947_first-migration.js
@@ -2,14 +2,29 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = async function (knex) {
+  await knex.schema
+    .createTable("projects", (tbl) => {
+      tbl.increments("project_id");
+    })
+    .createTable("resources", (tbl) => {
+      tbl.increments("resource_id");
+    })
+    .createTable("tasks", (tbl) => {
+      tbl.increments("task_id");
+    })
+    .createTable("project_resources", (tbl) => {
+      tbl.increments("project_resource_id");
+    });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("project_resources");
+  await knex.schema.dropTableIfExists("tasks");
+  await knex.schema.dropTableIfExists("resources");
+  await knex.schema.dropTableIfExists("projects");
 };
